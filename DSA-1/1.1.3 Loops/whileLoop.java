8eb08204b5467ee0447cf7550c654ebc94942aaa
Nawaz Danish <104252017+nawazdanish1996@users.noreply.github.com>
2022-04-25 23:28:04 +0530

    Add files via upload

---
+++ DSA-1/1.1.3 Loops/whileLoop.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class whileLoop{
+
+    public static void main(String[] args) {
+
+        System.out.println("Using: while loops ");
+        int i = 100;
+        while (i <= 200){
+            System.out.println(i);
+            i++;
+        }
+
+//        while (true){
+//            System.out.println("I am a Boy");
+//        }
+
+    }
+}
