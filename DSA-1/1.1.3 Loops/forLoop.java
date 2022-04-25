3beb388284f6cdf58e66cc93d2809f972f6a91f0
Nawaz Danish <104252017+nawazdanish1996@users.noreply.github.com>
2022-04-25 22:49:17 +0530

    Add files via upload

--- DSA-1/1.1.3 Loops/forLoop.java
+++ DSA-1/1.1.3 Loops/forLoop.java
@@ -3,6 +3,25 @@ package com.company;
 public class forLoop {
 
     public static void main(String[] args) {
-        System.out.println("Mister");
+//        for (int i = 1; i <= 5; i++){
+//            System.out.println("n");
+//        }
+
+//        2i = Even Numbers = 0, 2, 4, 6, 8
+//        2i+1 = Odd Numbers = 1, 3, 5, 7, 9
+
+//        int n = 3;
+//        for (int i=0; i<5; i++){
+//            System.out.println(2*i+1);
+//        }
+
+//        for (int i=5; i>=0; i--){
+//            System.out.println(i);
+//        }
+
+        for (int i = 5; i > 0; i--){
+            System.out.println(i);
+        }
+
     }
 }
---
+++ DSA-1/1.1.3 Loops/patternMaking.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class patternMaking {
+
+    public static void main(String[] args) {
+
+    }
+}
